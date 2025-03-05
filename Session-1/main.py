fc15b7c7b3b2d2bb309d496a7287bc2d3a11fc05
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2025-03-05 15:19:25 +0330

    Added: Session1/2 pdf, Session1 class example file | Modified: Session

---
+++ Session-1/main.py
@@ -0,0 +1,64 @@
+# def add(num1, num2):
+#     natije = num1 + num2
+#     return natije
+
+# def divide(addad1, addad2):
+#     return addad1 / addad2 
+
+
+# result = add(2, 5)
+
+# print(divide(result, 3))
+
+
+# def factorial(number):
+#     if number == 0:
+#         return 1
+#     else:
+#         return number * factorial(number - 1)
+
+
+# print(factorial(3))
+
+
+# number = [1, 2, 3, 4, 5]
+
+# squared_numbers = list(map(lambda n: n ** 2, number))
+
+# print(squared_numbers)
+
+
+# def add(num1, num2):
+#     result = num1 + num2
+#     return result
+
+# natije = add(2, 5)
+# print(f"${natije}")
+
+
+# def power2(n):
+#     return n ** 2
+
+# lambda
+# my_list = [1, 2, 3, 4, 5, 6]
+
+# new_list = list(map(lambda n: n ** 2, my_list))
+
+# print(new_list)
+
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+    
+
+# print(factorial(5))
+
+# try:
+#     user_input = int(input("Enter a number: "))
+#     print(user_input_hhh)
+# except (ValueError, NameError) as something:
+#     print(f"You must enter a number. {something}")
+# finally:
+#     print("This will always run")
