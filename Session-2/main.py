65ada8cc1fbbdee208772e5ae73de8cdc982a563
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2025-03-09 17:25:14 +0330

    Added example file

---
+++ Session-2/main.py
@@ -0,0 +1,30 @@
+# snake_case
+# camelCase
+# PascalCase
+
+
+# class Human:
+#     temper = 20
+
+#     def __init__(self, name, age, gender):
+#         self.name = name
+
+#     def introduce(self):
+#         print(f"Hello I am {self.name}")
+
+#     def change_your_name(self, new_name):
+#         self.name = new_name
+#         self.introduce()
+
+# p1 = Human("Matin", 19, "male")
+# p1.introduce()
+
+# p1.change_your_name("Reza")
+# print(Human.temper)
+
+l1 = [1, "matin", True, 2.5]
+
+integer1 = "hello"
+
+
+print(type([1, "matin", True, 2.5]))
